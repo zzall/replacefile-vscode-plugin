8a9d0927dae12636795de75b840e185f4414028c
张志阵 <1809642355@qq.com>
2022-06-13 14:33:34 +0800

    feat:带上.vsix文件,可直接拉下来使用

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
 # replacefile-vscode-plugin
+
+
 created for file operation
-=======
+
+
+
 将less或css文件的像素减半!
->>>>>>> 924afab (feat:初版vscode插件)
--- extension.js
+++ extension.js
@@ -3,14 +3,22 @@ const { toString, fromString } = require('uint8arrays');
 
 function activate(context) {
 	console.log('Congratulations, your extension "replacefile-vscode-plugin" is now active!');
-	let disposable = vscode.commands.registerCommand('replacefile', function (uri) {
-		vscode.workspace.fs.readFile(uri).then(res => {
-			const contentUTF8 = toString(res);
-			const replacedContentUTF8 = contentUTF8.replace(/(?<=[ \:\-\+\*\/\'\"\)\(\!)])(\d+)px(?=[ ;\-\+\*\/\'\"\)\(\!])/g, function (a, b) { return `${b / 2}px` });
-			vscode.workspace.fs.writeFile(uri, fromString(replacedContentUTF8)).then(() => {
+	let disposable = vscode.commands.registerTextEditorCommand('extension.replacefile', function (textEditor, edit, args) {
+		const currentUri = args || textEditor && textEditor.document && textEditor.document.uri
+		if (currentUri) {
+			vscode.workspace.fs.readFile(currentUri).then(res => {
+				const contentUTF8 = toString(res);
+				const replacedContentUTF8 = contentUTF8.replace(/(?<=[ \:\-\+\*\/\'\"\)\(\!)])(\d+)px(?=[ ;\-\+\*\/\'\"\)\(\!])/g, function (a, b) { return `${b / 2}px` });
+				vscode.workspace.fs.writeFile(currentUri, fromString(replacedContentUTF8)).then(() => {
+					vscode.window.showInformationMessage('文件替换成功');
+				})
 				vscode.window.showInformationMessage('文件替换成功');
+			}, err => {
+				vscode.window.showErrorMessage('fs.readFile失败：', err);
 			})
-		})
+		} else {
+			vscode.window.showErrorMessage('找不到文件uri');
+		}
 	});
 
 	context.subscriptions.push(disposable);
